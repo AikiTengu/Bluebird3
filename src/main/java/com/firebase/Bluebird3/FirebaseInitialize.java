c7ccb4352ede0a24cbc8c80cee3f336404a6f290
tengu <aiki_tengu@yahoo.com>
2020-12-24 12:49:44 +0200

    Initialized firebase connection

--- src/main/java/com/firebase/Bluebird3/FirebaseInitialize.java
+++ src/main/java/com/firebase/Bluebird3/FirebaseInitialize.java
@@ -3,14 +3,20 @@ package com.firebase.Bluebird3;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+@Service
 public class FirebaseInitialize {
 
-    public void initialize() {
+    @PostConstruct
+    public void initialize() throws FileNotFoundException, IOException {
         FileInputStream serviceAccount =
-                new FileInputStream("path/to/serviceAccountKey.json");
+                new FileInputStream("./Bluebird3.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
